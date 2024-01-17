8185fafd3245573a9fa43592962a779429cb588e
3ltigr0 <jjparksw@gmail.com>
2024-01-17 20:08:53 +0900

    Initial commit

---
+++ src/main/java/com/eltigro/memberinvitelink/MemberInviteLinkApplication.java
@@ -0,0 +1,13 @@
+package com.eltigro.memberinvitelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberInviteLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberInviteLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eltigro/memberinvitelink/MemberInviteLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eltigro.memberinvitelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberInviteLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
